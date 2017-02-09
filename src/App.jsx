6b1ab54680b219e529c63d44937d69d4973d1b33
Arman Kazemi <kas1er@hotmail.com>
2017-02-09 01:52:00 +0000

    server now succesfully receieves input from client, sends it back to all connected browsers and renders the data

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,7 @@ class App extends Component {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: []
     }
-    // this.addMessage = this.addMessage.bind(this);
-    // this.submitMessage = this.submitMessage.bind(this);
   }
-
-  // submitMessage(event) {
-  //   event
-  // }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
@@ -27,46 +21,43 @@ class App extends Component {
     }
     this.socket.onopen = function(event) { //same as "addEvenListener()"
       console.log("Client message: Connected to server");
-      // this.socket.send("Sample message");
-      // this.socket.send("Hello from client");
     }
 
     this.socket.onmessage = (event) => {
+      console.log("event", event);
       console.log("server said:", event.data)
       const message = JSON.parse(event.data);
-    }
-    // console.log('user', newMessage.username, 'said', newMessage.content);
+      console.log("message", message);
+      console.log("message.type", message.type)
+      console.log("this.state.data.messages", event.data.message);
 
-    // console.log('Connected to server');
-    // this.connection.onmessage = evt => {
-    //   this.setState({
-    //     messages: messages
-    //   })
-    // }
+
     // Add a new message to the list of messages in the data store
-    const newMessage = {id: 4, username: "Michelle", content: "Hello there!"};
+    const newMessage = {
+      id: message.id,
+      content: message.content,
+      username: message.username
+    };
     const messages = this.state.messages.concat(newMessage)
     // Update the state of the app component.
     // Calling setState will trigger a call to render() in App and all child components.
     this.setState({messages: messages})
+
   }
+}
 
   addMessage(content) {
-      const newMessage = {
-                          id: counter++,
-                          username: this.state.currentUser.name,
-                          content:content
-                         }
+    const newMessage = {
+                        username: this.state.currentUser.name,
+                        content: content
+                       }
 
-      // this.socket.send(event.target.value);
-      const messages = this.state.messages.concat(newMessage)
-
-      // console.log(event.target.value);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-      this.socket.send(JSON.stringify(newMessage));
-    }
+    // console.log(event.target.value);
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    console.log("newMessage:", newMessage);
+    this.socket.send(JSON.stringify(newMessage));
+  }
 
   render() {
     return (
